1d222d11358587bec56b5f770be3808d58c39120
Rai <mraihanzhafran.14@gmail.com>
2023-01-18 06:28:06 +0700

    making a few changes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include "assets/php/protocol.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="<?php echo $url; ?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="assets/css/index.css" rel="stylesheet">
     <title>Portfolio | MRZ</title>
 </head>

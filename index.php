90e4cedf642fd6e3792ad382c1f3298de080951a
Rai <mraihanzhafran.14@gmail.com>
2023-01-17 19:33:28 +0700

    adding a bunch of things

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Portfolio | MRZ</title>
+</head>
+
+<body>
+    <div class="side-navbar col-1">
+        <div id="list-example" class="list-group">
+            <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
+            <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+        </div>
+    </div>
+    <div class="col-8">
+        <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
+            <h4 id="list-item-1">Item 1</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem quia eius dolore amet culpa expedita ipsam aperiam sunt ab, aspernatur reiciendis vel quibusdam, laborum quas nihil repellat. Assumenda, vero sed.</p>
+            <h4 id="list-item-2">Item 2</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore delectus a doloribus illum omnis quod ab sequi, perferendis aliquid magni nemo voluptatum, blanditiis quaerat! Dolorum at exercitationem cupiditate voluptates totam?</p>
+            <h4 id="list-item-3">Item 3</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore delectus a doloribus illum omnis quod ab sequi, perferendis aliquid magni nemo voluptatum, blanditiis quaerat! Dolorum at exercitationem cupiditate voluptates totam?</p>
+            <h4 id="list-item-4">Item 4</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore delectus a doloribus illum omnis quod ab sequi, perferendis aliquid magni nemo voluptatum, blanditiis quaerat! Dolorum at exercitationem cupiditate voluptates totam?</p>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>

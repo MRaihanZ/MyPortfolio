175d5ce2df0c945907794e8de73ee00c59680d08
Rai <mraihanzhafran.14@gmail.com>
2023-01-25 18:35:18 +0700

    adding education div

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ include "assets/php/protocol.php";
             <div class="container pb-5">
                 <div class="row text-center">
                     <div class="col" data-aos="fade-up" data-aos-duration="1000">
-                        <h2>About Me</h2>
+                        <h2 class="fw-bold">About Me</h2>
                     </div>
                 </div>
                 <div class="row justify-content-between">
@@ -81,12 +81,13 @@ include "assets/php/protocol.php";
                         reiciendis fugit veritatis dignissimos minima! Quas vel est suscipit atque maxime porro provident!
                     </div>
                 </div>
-                <div class="row mt-5">
+                <p class="fs-3 text-center fw-bold mt-5">My Skill</p>
+                <div class="row mt-4">
                     <div class="col">
                         <div class="html">
                             <p>HTML</p>
-                            <div class="progress bg-white" role="progressbar" aria-label="Animated striped example" aria-valuenow="97" aria-valuemin="0" aria-valuemax="100">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 97%" data-aos="slide-right" data-aos-duration="1000">97%</div>
+                            <div class="progress bg-white" role="progressbar" aria-label="Animated striped example" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 99%" data-aos="slide-right" data-aos-duration="1000">99%</div>
                             </div>
                         </div>
                         <div class="css mt-4">
@@ -110,10 +111,17 @@ include "assets/php/protocol.php";
                             </div>
                         </div>
                     </div>
+                    <div class="mysql mt-4 text-center">
+                        <p>Mysql</p>
+                        <div class="progress bg-white" role="progressbar" aria-label="Animated striped example" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" style="width: 90%" data-aos="slide-right" data-aos-duration="1000">90%</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="row">
+                    <p class="fs-3 text-center fw-bold mt-5">Library & Framework</p>
                     <div class="col">
-                        <div class="bootstrap mt-4">
+                        <div class="bootstrap mt-3">
                             <p>Bootstrap</p>
                             <div class="progress bg-white" role="progressbar" aria-label="Animated striped example" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100">
                                 <div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" style="width: 95%" data-aos="slide-right" data-aos-duration="1000">95%</div>
@@ -128,13 +136,14 @@ include "assets/php/protocol.php";
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="aos mt-4 text-center">
-                    <p>AOS</p>
-                    <div class="progress bg-white" role="progressbar" aria-label="Animated striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 100%" data-aos="slide-right" data-aos-duration="1000">100%</div>
+                    <div class="aos mt-4 text-center">
+                        <p>AOS</p>
+                        <div class="progress bg-white" role="progressbar" aria-label="Animated striped example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar progress-bar-striped progress-bar-animated" style="width: 100%" data-aos="slide-right" data-aos-duration="1000">100%</div>
+                        </div>
                     </div>
                 </div>
+
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#ffffff" fill-opacity="1" d="M0,160L0,64L96,64L96,64L192,64L192,96L288,96L288,128L384,128L384,192L480,192L480,160L576,160L576,96L672,96L672,32L768,32L768,64L864,64L864,128L960,128L960,192L1056,192L1056,224L1152,224L1152,96L1248,96L1248,128L1344,128L1344,32L1440,32L1440,320L1344,320L1344,320L1248,320L1248,320L1152,320L1152,320L1056,320L1056,320L960,320L960,320L864,320L864,320L768,320L768,320L672,320L672,320L576,320L576,320L480,320L480,320L384,320L384,320L288,320L288,320L192,320L192,320L96,320L96,320L0,320L0,320Z">
@@ -148,15 +157,61 @@ include "assets/php/protocol.php";
                 </path>
             </svg>
             <div class="container">
-                <div class="sd bg-white">
-                    <div class="row">
-                        <div class="col-5">
-                            <img class="img-thumbnail" src="assets/img/sdn-jatiasih-vi.jpg" alt="SDN Jatiasih VI" width="200">
-                        </div>
-                        <div class="col">
-                            <h2>Elementary School</h2>
-                            <h5>2008-2014</h5>
-                        </div>
+                <div class="progress-stacked bg-white mx-auto mb-5">
+                    <div class="progress" role="progressbar" aria-label="Segment one" aria-valuenow="25 Animated striped example" aria-valuemin="0" aria-valuemax="100" style="width: 25%">
+                        <div class="progress-bar bg-danger progress-bar-striped progress-bar-animated" data-aos="fade-right" data-aos-duration="200">Elementary School 100%</div>
+                    </div>
+                    <div class="progress" role="progressbar" aria-label="Segment two" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%">
+                        <div class="progress-bar progress-bar-striped progress-bar-animated" data-aos="fade-right" data-aos-delay="150" data-aos-duration="200">Junior High School 100%</div>
+                    </div>
+                    <div class="progress" role="progressbar" aria-label="Segment three" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%">
+                        <div class="progress-bar bg-secondary progress-bar-striped progress-bar-animated" data-aos="fade-right" data-aos-delay="250" data-aos-duration="200">Vocational School 100%</div>
+                    </div>
+                    <div class="progress" role="progressbar" aria-label="Segment three" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 10%">
+                        <div class="progress-bar bg-purple progress-bar-striped progress-bar-animated" data-aos="fade-right" data-aos-delay="350" data-aos-duration="200">university 10%</div>
+                    </div>
+                </div>
+                <div class="card mx-auto" data-aos="fade-up" style="width: 10rem;">
+                    <img class="card-img-top img-thumbnail" src="assets/img/sdn-jatiasih-vi.jpg" alt="SDN Jatiasih VI">
+                    <div class="card-body">
+                        <b>
+                            <p class="card-text fs-5 text-center border-bottom">Elementary School</p>
+                        </b>
+                        <br>
+                        <h5 class="text-center">2008-2014</h5>
+                    </div>
+                </div>
+                <div class="line-connector bg-black mx-auto my-3" data-aos="fade-up"></div>
+                <div class="card mx-auto" data-aos="fade-up" style="width: 10rem;">
+                    <img class="card-img-top img-thumbnail" src="assets/img/smp-genr.jpg" alt="SDN Jatiasih VI">
+                    <div class="card-body">
+                        <b>
+                            <p class="card-text fs-5 text-center border-bottom">Junior High School</p>
+                        </b>
+                        <br>
+                        <h5 class="text-center">2008-2014</h5>
+                    </div>
+                </div>
+                <div class="line-connector bg-black mx-auto my-3" data-aos="fade-up"></div>
+                <div class="card mx-auto" data-aos="fade-up" style="width: 10rem;">
+                    <img class="card-img-top img-thumbnail" src="assets/img/sekolah65.jpg" alt="SDN Jatiasih VI">
+                    <div class="card-body">
+                        <b>
+                            <p class="card-text fs-5 text-center border-bottom">Vocational School</p>
+                        </b>
+                        <br>
+                        <h5 class="text-center">2008-2014</h5>
+                    </div>
+                </div>
+                <div class="line-connector bg-black mx-auto my-3" data-aos="fade-up"></div>
+                <div class="card mx-auto mb-5" data-aos="fade-up" style="width: 10rem;">
+                    <img class="card-img-top img-thumbnail" src="assets/img/universitas-gunadarma.jpg" alt="SDN Jatiasih VI">
+                    <div class="card-body">
+                        <b>
+                            <p class="card-text fs-5 text-center border-bottom">university</p>
+                        </b>
+                        <br>
+                        <h5 class="text-center">2008-2014</h5>
                     </div>
                 </div>
             </div>
@@ -170,6 +225,7 @@ include "assets/php/protocol.php";
                 <path fill="#ffffff" fill-opacity="1" d="M0,160L0,64L96,64L96,64L192,64L192,96L288,96L288,128L384,128L384,192L480,192L480,160L576,160L576,96L672,96L672,32L768,32L768,64L864,64L864,128L960,128L960,192L1056,192L1056,224L1152,224L1152,96L1248,96L1248,128L1344,128L1344,32L1440,32L1440,0L1344,0L1344,0L1248,0L1248,0L1152,0L1152,0L1056,0L1056,0L960,0L960,0L864,0L864,0L768,0L768,0L672,0L672,0L576,0L576,0L480,0L480,0L384,0L384,0L288,0L288,0L192,0L192,0L96,0L96,0L0,0L0,0Z">
                 </path>
             </svg>
+
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                 <path fill="#ffffff" fill-opacity="1" d="M0,160L0,64L96,64L96,64L192,64L192,96L288,96L288,128L384,128L384,192L480,192L480,160L576,160L576,96L672,96L672,32L768,32L768,64L864,64L864,128L960,128L960,192L1056,192L1056,224L1152,224L1152,96L1248,96L1248,128L1344,128L1344,32L1440,32L1440,320L1344,320L1344,320L1248,320L1248,320L1152,320L1152,320L1056,320L1056,320L960,320L960,320L864,320L864,320L768,320L768,320L672,320L672,320L576,320L576,320L480,320L480,320L384,320L384,320L288,320L288,320L192,320L192,320L96,320L96,320L0,320L0,320Z">
                 </path>
@@ -197,6 +253,17 @@ include "assets/php/protocol.php";
             delay: 0.5,
             duration: 1,
             borderRadius: 100,
+        });
+        document.querySelectorAll(".box").forEach(function(box) {
+            box.addEventListener("click", function() {
+                gsap.to(".box", {
+                    duration: 0.5,
+                    opacity: 0,
+                    y: -100,
+                    stagger: 0.1,
+                    ease: "back.in"
+                });
+            });
         });
     </script>
 </body>
